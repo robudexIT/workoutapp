7d0a824927eb5639cfb8d202367af88052f92522
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2022-08-11 04:31:36 +0800

    latest commit on cookie branch

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -12,9 +12,9 @@ const store = createStore({
         return {
             fromMainStore: '',
             // apiAddr: 'http://localhost:3000'
-             apiAddr: 'http://210.1.86.214:3000'
+            //  apiAddr: 'http://210.1.86.214:3000'
             //  apiAddr:'http://ec2-54-213-10-154.us-west-2.compute.amazonaws.com:3000'
-            // apiAddr: 'https://498scwkbai.execute-api.us-west-2.amazonaws.com/dev'
+            apiAddr: 'https://4cvpevlkn5.execute-api.us-west-2.amazonaws.com/dev'
         }
     },
     getters:{
