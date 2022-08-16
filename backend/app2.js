6529eabfd6888730889795e2f53e3ce8d3084f39
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2022-08-17 03:18:16 +0800

    adding to memcached branch

--- backend/app2.js
+++ backend/app2.js
@@ -1,62 +1,32 @@
 const express = require('express')
-require('dotenv').config()
 const app = express()
-const db = require('./config/database')
-// const workoutRoutes = require('./routes/workoutroutes')
-// const authRoutes = require('./routes/authRoutes')
-const jwt = require('jsonwebtoken')
-const tokenSecret = process.env.TOKEN_SECRET
-const cookieParser = require('cookie-parser')
+const Memcached = require('memcached')
+const memcached = new Memcached('memcluster.lhvqbe.0001.usw2.cache.amazonaws.com:11211')
+memcached.on('failure', function( details ){ sys.error( "Server " + details.server + "went down due to: " + details.messages.join( '' ) ) });
+memcached.on('reconnecting', function( details ){ sys.debug( "Total downtime caused by server " + details.server + " :" + details.totalDownTime + "ms")});
 
-const PORT = process.env.PORT
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
- app.use(cookieParser())
-app.use(setCustomHeaders)
-// app.use(authRoutes)
-// app.use(workoutRoutes)
-
-app.get('/', (req, res) => {
-    console.log('it came here')
-    res.cookie('name', 'geeksforgeeks')
-    res.json({message:'Cookie is set'})
-
+const PORT = 3001
+app.get('/set', (req, res, next) => {
+  memcached.set('name', 'Rogmer', 30, function(err){
+    if(err){
+      console.log(err)
+      return
+    }
+    res.end('Adding name to the memcached')
+  })
 })
-app.get('/token', (req,res, next) => {
-    const cookie = req.cookies.name
-    console.log(cookie)
-    res.send(cookie)
+apt.get('/get', (req, res, next) => {
+  memcached.get('name', function(err, data) {
+    if(err){
+      console.log(err)
+      return
+    }
+    console.log(data)
+    res.end(data)
+  })
 })
-const connectDB = async() => {
-     try{
-        await db.authenticate()
-        console.log('Successfully Connect to db')
-        app.listen(PORT, () => {
-            console.log('App is running on port ', PORT)
-        })
-     }catch(error){
-        console.log(error)
-     }
-    
-}
+app.listen(PORT, () => {
+  console.log('App is running and listening on port',PORT)
+})
 
 
-function setCustomHeaders(req, res, next) {
-     // Website you wish to allow to connect
-     res.setHeader('Access-Control-Allow-Origin', '*');
-
-     // Request methods you wish to allow
-     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
- 
-     // Request headers you wish to allow
-   //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
-     res.setHeader('Access-Control-Allow-Headers', '*');
- 
-     // Set to true if you need the website to include cookies in the requests sent
-     // to the API (e.g. in case you use sessions)
-     res.setHeader('Access-Control-Allow-Credentials', true);
- 
-     // Pass to next layer of middleware
-     next();
-}
-connectDB()
