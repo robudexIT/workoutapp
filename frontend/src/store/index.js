09eb68799ed0aab7cb016ae62e6681bbd0f2dcbb
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2022-08-23 04:57:57 +0800

    commit to memcached

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -14,7 +14,7 @@ const store = createStore({
             //  apiAddr: 'http://localhost:3000' // for backend is also on local
             //   apiAddr: 'http://210.1.86.214:3000' // backend is on another server/domain. cookie cannot send/recieve via http it securet https (connection)
             
-             apiAddr: 'https://n2xfnt3nml.execute-api.us-east-1.amazonaws.com/latest' // aws api-gateway endpoint  act as a proxy for backend to allow sending and recieve cookie 
+             apiAddr: 'https://4cvpevlkn5.execute-api.us-west-2.amazonaws.com/dev' // aws api-gateway endpoint  act as a proxy for backend to allow sending and recieve cookie 
         }
     },
     getters:{
