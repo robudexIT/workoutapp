dfe8b6134b902289b7742e45c1be257bf7209c96
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2022-08-24 01:15:23 +0800

    commit in memcached branch

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -139,7 +139,7 @@ module.exports.signoutUser = async(req, res, next) => {
         const getSaveTokens = await memcached.get(req.username)
         const tokens = JSON.parse(getSaveTokens)
         tokens.refreshTokenList = tokens.refreshTokenList.filter(rf => rf != req.refreshToken)
-        tokens.accessTokenList = tokens.accessTokenList.filter(actoken => actoken != req.token)
+        tokens.accessTokenList = tokens.accessTokenList.filter(actoken => actoken != req.accesToken)
         
         //delete user completly on the memcached if there no tokens in the list
         if(tokens.refreshTokenList.length == 0 && tokens.accessTokenList.length == 0){
